42bfcdfbbea511c1b5e6db13e0534eeec4dbcb7e
Laboratorio101 <Laboratorio101@LAB102-24>
2020-02-11 23:16:08 -0200

    First commit

---
+++ src/view/Principal.java
@@ -0,0 +1,15 @@
+package view;
+
+import controller.OperacoesController;
+
+public class Principal 
+{
+	public static void main(String[] args) 
+	{
+		OperacoesController opController = new OperacoesController();
+//		opController.OpString();
+//		opController.OpBuffer();
+		String frase = "Sejam bem vindos";
+		opController.opFrase(frase);
+	}
+}
